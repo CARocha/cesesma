fadc87b2a3201cd555ac1e38195aea3575ceda8e
sacrac <crocha09.09@gmail.com>
2013-09-06 16:22:48 -0600

    fix lista de publicaciones

--- publicaciones/urls.py
+++ publicaciones/urls.py
@@ -6,7 +6,7 @@ from .views import filtro_publicaciones
 urlpatterns = patterns('',
         url(r'^lista/$',  ListView.as_view(model=Publicaciones,
                                                                queryset=Publicaciones.objects.all(),
-                                                               paginate_by=8), 
+                                                               paginate_by=7), 
                                                                name='publicaciones_lista'),
         
         url(r'^(?P<slug>[-_\w]+)/$', DetailView.as_view(model=Publicaciones,
